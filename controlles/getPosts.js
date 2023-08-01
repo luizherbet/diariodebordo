71f9e0ba7d7616107b44de1444b4f3012c34e68b
luizherbet <107811832+luizherbet@users.noreply.github.com>
2023-08-01 11:54:21 -0300

    getPosts.js

--- controlles/getPosts.js
+++ controlles/getPosts.js
@@ -1,7 +1,7 @@
 const DiarioPost = require('../models/DiarioPost');
 
 module.exports = async (req, res) => {
-  const diarioposts = await DiarioPost.find({}).populate('userid');
+  const diarioposts = await DiarioPost.find({ userid: usernotes });
   console.log(req.params.id);
   res.render('posts', { diarioposts });
 };
